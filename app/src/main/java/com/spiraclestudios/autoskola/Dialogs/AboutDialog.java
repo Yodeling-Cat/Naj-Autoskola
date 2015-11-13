86ad0510cb613017c2b777974d27161c0f70f3cb
Tomáš Beňo <benjiko99@gmail.com>
2015-11-13 12:24:14 +0100

    - Added links to social networks

--- app/src/main/java/com/spiraclestudios/autoskola/Dialogs/AboutDialog.java
+++ app/src/main/java/com/spiraclestudios/autoskola/Dialogs/AboutDialog.java
@@ -1,6 +1,8 @@
 package com.spiraclestudios.autoskola.Dialogs;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -13,9 +15,11 @@ import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.spiraclestudios.autoskola.AutoskolaApplication;
 import com.spiraclestudios.autoskola.BuildConfig;
+import com.spiraclestudios.autoskola.Helper;
 import com.spiraclestudios.autoskola.R;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by benji on 5/10/2015.
@@ -35,6 +39,11 @@ public class AboutDialog extends DialogFragment {
 
         TextView info_values = ButterKnife.findById(view, R.id.info_values);
         ImageView app_icon = ButterKnife.findById(view, R.id.app_icon);
+        // Social networks
+        ImageView facebook = ButterKnife.findById(view, R.id.facebook);
+        ImageView twitter = ButterKnife.findById(view, R.id.twitter);
+        ImageView youtube = ButterKnife.findById(view, R.id.youtube);
+        ImageView google_play = ButterKnife.findById(view, R.id.google_play);
 
         info_values.setText(
                 String.format(res.getString(R.string.dialog_about_info_values),
@@ -52,6 +61,38 @@ public class AboutDialog extends DialogFragment {
                 fragment.show(getActivity().getSupportFragmentManager(), "DevTools");
                 dismiss();
                 return true;
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Helper.facebookURL));
+                startActivity(browserIntent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Helper.twitterURL));
+                startActivity(browserIntent);
+            }
+        });
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Helper.youtubeURL));
+                startActivity(browserIntent);
+            }
+        });
+
+        google_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Helper.googlePlayURL));
+                startActivity(browserIntent);
             }
         });
 
