cb89e1ea186e180b1b1fca55b9ff61e808b19b3c
Tomáš Beňo <benjiko99@gmail.com>
2015-11-21 14:08:08 +0100

    Changed project structure

---
+++ Android/app/src/main/java/com/spiraclestudios/autoskola/Activities/BaseActivity.java
@@ -0,0 +1,117 @@
+package com.spiraclestudios.autoskola.Activities;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.spiraclestudios.autoskola.AnalyticsTrackers;
+import com.spiraclestudios.autoskola.Dialogs.AboutDialog;
+import com.spiraclestudios.autoskola.Interfaces.IBaseActivity;
+import com.spiraclestudios.autoskola.R;
+
+/**
+ * Created by benji on 08/11/2015.
+ */
+public class BaseActivity extends AppCompatActivity
+        implements IBaseActivity, NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "BaseActivity";
+    public String mActivityName;
+    private Tracker mTracker;
+
+    public BaseActivity() {
+        mActivityName = getActivityName();
+    }
+
+    public String getActivityName() {
+        return mActivityName;
+    }
+
+    public Tracker getTracker() {
+        if (mTracker == null) {
+            mTracker = AnalyticsTrackers.getInstance().get(AnalyticsTrackers.Target.APP);
+        }
+        return mTracker;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "Setting analytics tracker screen name: " + getActivityName());
+        getTracker().setScreenName(getActivityName());
+        getTracker().send(new HitBuilders.ScreenViewBuilder().build());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer != null && drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_testy) {
+            if (getActivityName() == "MainActivity") {
+                return true;
+            }
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_novinky) {
+            return true;
+        } else if (id == R.id.nav_dopravne_znacky) {
+            if (getActivityName() == "RoadSignsListActivity") {
+                return true;
+            }
+            // TODO: Re-enable Road Signs activity
+            //Intent intent = new Intent(this, RoadSignsListActivity.class);
+            //startActivity(intent);
+        } else if (id == R.id.nav_vyhlaska) {
+            return true;
+        } else if (id == R.id.nav_najst_autoskolu) {
+            return true;
+        } else if (id == R.id.nav_nastavenia) {
+            if (getActivityName() == "SettingsActivity") {
+                return true;
+            }
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_pomoc_a_pripomienky) {
+            if (getActivityName() == "FeedbackActivity") {
+                return true;
+            }
+            getTracker().send(new HitBuilders.EventBuilder()
+                    .setCategory("Navigation")
+                    .setAction("Pomoc a Pripomienky")
+                    .build());
+
+            Intent intent = new Intent(this, FeedbackActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_o_aplikacii) {
+            getTracker().send(new HitBuilders.EventBuilder()
+                    .setCategory("Navigation")
+                    .setAction("O Aplikácii")
+                    .build());
+
+            DialogFragment dialog = new AboutDialog();
+            dialog.show(getSupportFragmentManager(), "About");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ Android/app/src/main/java/com/spiraclestudios/autoskola/Helper.java
@@ -0,0 +1,141 @@
+package com.spiraclestudios.autoskola;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.zplesac.connectifty.Connectify;
+import com.zplesac.connectifty.cache.ConnectifyCache;
+import com.zplesac.connectifty.interfaces.ConnectivityChangeListener;
+import com.zplesac.connectifty.models.ConnectifyEvent;
+import com.zplesac.connectifty.models.ConnectifyState;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by benji on 14/10/2015.
+ */
+public class Helper {
+    // Social links
+    public static final String webURL = "http://spiraclestudios.com";
+    public static final String facebookURL = "https://facebook.com/spiraclestudios";
+    public static final String twitterURL = "https://twitter.com/SpiracleStudios";
+    public static final String youtubeURL = "https://youtube.com/channel/UCYF2X1mTodkkRkKTp0ER2aw";
+    public static final String googlePlayURL = "https://play.google.com/store/apps/developer?id=Spiracle+Studios";
+
+    public static boolean demoMode = false;
+
+    public enum Groups {
+        AB,
+        CDT
+    }
+
+    // Returns 0 (A,B) if index is 1-35 and 1 (C,D,T) if index is greater than 35
+    public static Groups getGroupFromTestIndex(int index) {
+        return (index > 35) ? Groups.CDT : Groups.AB;
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /*@Override
+    public void onStop() {
+        super.onStop();
+        Connectify.getInstance().unregisterFromConnectivityEvents(this);
+    }
+
+    @Override
+    public void onConnectionChange(ConnectifyEvent event) {
+        if (event.getState() == ConnectifyState.CONNECTED) {
+            subscribe.setEnabled(true);
+            connectivity_error.setVisibility(View.GONE);
+        } else {
+            subscribe.setEnabled(false);
+            connectivity_error.setVisibility(View.VISIBLE);
+        }
+    }*/
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            ConnectifyCache.clearLastNetworkState(this);
+        }
+    }*/
+
+    public static void loadAd(final Context context, final AdView adView) {
+        if (demoMode) {
+            return;
+        }
+
+        //Connectify.getInstance().registerForConnectivityEvents(context, helper);
+
+        if (isOnline(context)) {
+            SharedPreferences prefs = PreferenceManager
+                    .getDefaultSharedPreferences(context.getApplicationContext());
+
+            AdRequest.Builder builder = new AdRequest.Builder()
+                    // [Ben's Devices]
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .addTestDevice("A053777425A9926103BE02DE879DA5A1") // Galaxy Note
+                    .addTestDevice("B0FF4D1DC8FED5463A805EA5860E577C") // Galaxy S3 Mini
+                            // TODO: .addTestDevice("") // Asus Memo Pad 10
+                            // [Genymotion]
+                    .addTestDevice("E4FAF36C23D3DD95FF1C53E4D55E81C7") // Google Nexus 5
+                    .addTestDevice("205C906BD72C2FA820456260F0BE0FA5") // Samsung Galaxy S2
+                    .addTestDevice("AF16642C9258EC370AE63F311FF12DCF") // Samsung Galaxy S3
+                    .addTestDevice("A27D5C846DF77ECC008E8DC2A84D5DBA") // Custom Tablet
+                            // Targeting
+                    .setGender(prefs.getInt("user_gender", 0));
+
+            if (prefs.contains("user_birth_year")) {
+                builder.setBirthday(new GregorianCalendar(prefs
+                        .getInt("user_birth_year", 2000), 1, 1).getTime());
+            }
+
+            adView.loadAd(builder.build());
+        } else {
+            // TODO: Implement proper network check for ads
+            // If there is no internet connection, check every 30 seconds if connection changed
+            Handler handler = new Handler();
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    loadAd(context, adView);
+                }
+            };
+            handler.postDelayed(runnable, 30000);
+        }
+    }
+
+    // Handle setting the night theme
+    public static void setTheme(Context context) {
+        if (PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean("night_theme_switch", false)) {
+            if (PreferenceManager.getDefaultSharedPreferences(context)
+                    .getBoolean("amoled_mode_switch", false))
+                context.setTheme(R.style.MyTheme_Dark_AMOLED);
+            else
+                context.setTheme(R.style.MyTheme_Dark);
+        } else
+            context.setTheme(R.style.MyTheme_Light);
+    }
+
+    public static void setDemoMode(boolean value) {
+        demoMode = value;
+    }
+}
