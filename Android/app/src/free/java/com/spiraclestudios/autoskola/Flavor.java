49f3893481bcc64d7247803ed8328612ddc08100
Tomáš Beňo <benjiko99@gmail.com>
2017-03-30 20:28:38 +0200

    deleted useless Flavor class

--- Android/app/src/free/java/com/spiraclestudios/autoskola/Flavor.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015-2017. Spiracle Software. All Rights Reserved.
-
-package com.spiraclestudios.autoskola;
-
-public class Flavor implements IFlavor {
-    public String startRoadSignsActivity() {
-        return "";
-    }
-}
