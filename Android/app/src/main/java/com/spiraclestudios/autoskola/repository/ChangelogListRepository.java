2506e17f71a2612479316cf8f13012e846dea137
Tomáš Beňo <benjiko99@gmail.com>
2017-09-27 02:11:44 +0200

    Modified changelogs

--- Android/app/src/main/java/com/spiraclestudios/autoskola/repository/ChangelogListRepository.java
+++ Android/app/src/main/java/com/spiraclestudios/autoskola/repository/ChangelogListRepository.java
@@ -23,9 +23,10 @@ public class ChangelogListRepository {
   static {
     ArrayList<Changelog> changelog_12 = new ArrayList<>();
     changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__most_points));
+    changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__successful_tests_are_highlighted));
     changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__immediate_score));
-    changelog_12.add(new Changelog(CHANGED, R.string.changelog__text__changes_in_v12__changed__answer_colors));
     changelog_12.add(new Changelog(CHANGED, R.string.changelog__text__changes_in_v12__changed__smoother_theme_switching));
+    changelog_12.add(new Changelog(CHANGED, R.string.changelog__text__changes_in_v12__changed__improved_look));
     changelog_12.add(new Changelog(FIXED, R.string.changelog__text__changes_in_v12__fixed__history_delete_all));
     changelogMap.put(12, changelog_12);
   }
