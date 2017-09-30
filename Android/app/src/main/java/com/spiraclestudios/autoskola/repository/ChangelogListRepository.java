13b1a8f102f72ad07645c82db7c206c2c574787f
Tomáš Beňo <benjiko99@gmail.com>
2017-09-30 20:04:48 +0200

    Added smooth scroll to the changelog

--- Android/app/src/main/java/com/spiraclestudios/autoskola/repository/ChangelogListRepository.java
+++ Android/app/src/main/java/com/spiraclestudios/autoskola/repository/ChangelogListRepository.java
@@ -25,6 +25,7 @@ public class ChangelogListRepository {
     changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__most_points));
     changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__successful_tests_are_highlighted));
     changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__immediate_score));
+    changelog_12.add(new Changelog(ADDED, R.string.changelog__text__changes_in_v12__added__smooth_scroll));
     changelog_12.add(new Changelog(CHANGED, R.string.changelog__text__changes_in_v12__changed__smoother_theme_switching));
     changelog_12.add(new Changelog(CHANGED, R.string.changelog__text__changes_in_v12__changed__improved_look));
     changelog_12.add(new Changelog(FIXED, R.string.changelog__text__changes_in_v12__fixed__road_signs));
