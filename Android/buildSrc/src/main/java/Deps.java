5efbea1a1977e67589032c38d5fa54e8df05b7ca
Tomáš Beňo <benjiko99@gmail.com>
2017-10-01 23:45:22 +0200

    Define dependencies in code, using buildSrc

---
+++ Android/buildSrc/src/main/java/Deps.java
@@ -0,0 +1,51 @@
+// Copyright (c) 2015-2017. Spiracle Software. All Rights Reserved.
+
+public class Deps {
+
+  public static final String androidPlugin = "com.android.tools.build:gradle:3.0.0-beta6";
+  public static final String googleServices = "com.google.gms:google-services:3.0.0";
+
+  //public static final String kotlinVersion = "1.1.50";
+  //public static final String kotlinRuntime = "org.jetbrains.kotlin:kotlin-stdlib:" + kotlinVersion;
+  //public static final String kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:" + kotlinVersion;
+
+  public static final String supportVersion = "26.1.0";
+  public static final String supportAppCompat = "com.android.support:appcompat-v7:" + supportVersion;
+  public static final String supportV4 = "com.android.support:support-v4:" + supportVersion;
+  public static final String supportV13 = "com.android.support:support-v13:" + supportVersion;
+  public static final String supportDesign = "com.android.support:design:" + supportVersion;
+  public static final String supportRecyclerView = "com.android.support:recyclerview-v7:" + supportVersion;
+  public static final String supportCardView = "com.android.support:cardview-v7:" + supportVersion;
+  public static final String supportGridLayout = "com.android.support:gridlayout-v7:" + supportVersion;
+  //public static final String supportConstraintLayout = "com.android.support.constraint:constraint-layout:1.0.1";
+
+  public static final String firebaseVersion = "11.4.0";
+  public static final String firebaseCore = "com.google.firebase:firebase-core:" + firebaseVersion;
+  public static final String firebaseAnalytics = "com.google.firebase:firebase-analytics:" + firebaseVersion;
+  public static final String firebaseAds = "com.google.firebase:firebase-ads:" + firebaseVersion;
+  public static final String firebaseInvites = "com.google.firebase:firebase-invites:" + firebaseVersion;
+
+  public static final String crashlytics = "com.crashlytics.sdk.android:crashlytics:2.6.8@aar";
+  public static final String fabricAnswers = "com.crashlytics.sdk.android:answers:1.3.13@aar";
+
+  public static final String leakCanaryVersion = "1.5.1";
+  public static final String leakCanary = "com.squareup.leakcanary:leakcanary-android:" + leakCanaryVersion;
+  public static final String leakCanaryNoOp = "com.squareup.leakcanary:leakcanary-android-no-op:" + leakCanaryVersion;
+
+  public static final String fastAdapterVersion = "2.5.2@aar";
+  public static final String fastAdapter = "com.mikepenz:fastadapter:" + fastAdapterVersion;
+  public static final String fastAdapterCommons = "com.mikepenz:fastadapter-commons:" + fastAdapterVersion;
+  public static final String fastAdapterExtensions = "com.mikepenz:fastadapter-extensions:" + fastAdapterVersion;
+
+  public static final String butterknifeVersion = "8.5.1";
+  public static final String butterknife = "com.jakewharton:butterknife:" + butterknifeVersion;
+  public static final String butterknifeAnnotations = "com.jakewharton:butterknife-compiler:" + butterknifeVersion;
+
+  public static final String stetho = "com.facebook.stetho:stetho:1.5.0";
+  public static final String guava = "com.google.guava:guava:23.0-android";
+  public static final String timber = "com.jakewharton.timber:timber:4.5.1";
+  public static final String connectionbuddy = "com.zplesac:connectionbuddy:1.4.0@aar";
+  public static final String glide = "com.github.bumptech.glide:glide:3.7.0";
+  public static final String multidex = "com.android.support:multidex:1.0.2";
+  public static final String aboutLibraries = "com.mikepenz:aboutlibraries:5.9.7@aar";
+}
