ebcba506fd6d04f214776e7f41d0190b6ca86227
Tomáš Beňo <benjiko99@gmail.com>
2017-10-03 21:08:16 +0200

    Added some screens to the ScreenFlowController

--- Android/app/src/main/java/com/spiraclestudios/autoskola/platform/ScreenFlowController.java
+++ Android/app/src/main/java/com/spiraclestudios/autoskola/platform/ScreenFlowController.java
@@ -4,8 +4,14 @@ package com.spiraclestudios.autoskola.platform;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import com.spiraclestudios.autoskola.framework.platform.Analytics;
+import com.spiraclestudios.autoskola.presentation.ui.activities.FeedbackActivity;
 import com.spiraclestudios.autoskola.presentation.ui.activities.HistoryActivity;
 import com.spiraclestudios.autoskola.presentation.ui.activities.HomeActivity;
+import com.spiraclestudios.autoskola.presentation.ui.activities.InformationActivity;
+import com.spiraclestudios.autoskola.presentation.ui.activities.RoadSignsCategoriesActivity;
+import com.spiraclestudios.autoskola.presentation.ui.activities.SettingsActivity;
 
 public class ScreenFlowController {
 
@@ -15,5 +21,23 @@ public class ScreenFlowController {
 
   public static void showHistoryActivity(Activity activity) {
     activity.startActivity(new Intent(activity.getApplicationContext(), HistoryActivity.class));
+  }
+
+  public static void showRoadSignsCategoriesActivity(Activity activity) {
+    activity.startActivity(new Intent(activity.getApplicationContext(), RoadSignsCategoriesActivity.class));
+  }
+
+  public static void showSettingsActivity(Activity activity) {
+    activity.startActivity(new Intent(activity.getApplicationContext(), SettingsActivity.class));
+  }
+
+  public static void showFeedbackActivity(Activity activity) {
+    activity.startActivity(new Intent(activity.getApplicationContext(), FeedbackActivity.class));
+  }
+
+  public static void showInformationActivity(Activity activity) {
+    Bundle bundle = new Bundle();
+    Analytics.getInstance(activity).logEvent("nav_about", bundle);
+    activity.startActivity(new Intent(activity.getApplicationContext(), InformationActivity.class));
   }
 }
